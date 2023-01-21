4868e732743eee329c23100ec165d0fbff0c29ce
dongfu <dongfu.jdf@gmail.com>
2023-01-21 00:50:51 -0800

    update for submisstion

--- src/baseline/mlm_score.sh
+++ src/baseline/mlm_score.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --time=56:00:00
+#SBATCH --time=10:00:00
 #SBATCH --job-name=mlm_score
 #SBATCH --output ../../jobs/%j.out
-#SBATCH --nodelist=ink-molly
-#SBATCH --gres=gpu:1080:2
+#SBATCH --nodelist=ink-ron
+#SBATCH --gres=gpu:2
 #SBATCH --qos=general
 #SBATCH -n 1
 
 nvidia-smi
 
-dataset="cnndm"
+dataset="cnndm_gpt3"
 set="test"
-generation_model="pegasus_cnndm"
-generation_method="top_k_sampling"
+generation_model="gpt3"
+generation_method="top_p_sampling"
 
 candidate_file="../../data/${dataset}/${set}/${generation_method}/candidates_${generation_model}.pkl"
 save_path="../../data/${dataset}/${set}/${generation_method}/cand_scores_${generation_model}_mlm.pkl"
